7790e260968ff259a5104a1681f42bf698cd729f
Marshall Cyrille Marcelo <mcam49856@gmail.com>
2026-01-11 16:25:59 +0800

    placeholders

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <header></header>
+      <main className="mx-auto h-dvh max-w-75">
+        <div>Home</div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
